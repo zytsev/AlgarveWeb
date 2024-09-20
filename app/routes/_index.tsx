1be877761f48b4ff7cfd74f8999cf897e8cb04f1
Evgeny Zaytsev <zaes2000@mail.ru>
2024-09-20 10:04:46 +0100

    feat: correct header

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ export default function Index() {
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Algarve Web</span>
+            Welcome to Algarve Web
           </h1>
           
         </header>
