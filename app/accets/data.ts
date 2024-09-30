b0eb5013def40b8932d5df4f422a018e73bdc351
Evgeny Zaytsev <zaes2000@mail.ru>
2024-09-30 17:21:31 +0100

    feat: add questions block

---
+++ app/accets/data.ts
@@ -0,0 +1,58 @@
+export const navItems = ["Primeira", "Serviços", "Contato"];
+export const resultsItems = [
+  { title: "20+", text: "Projetos de sucesso" },
+  { title: "30+", text: "Clientes felizes" },
+  { title: "24/7", text: "Suporte ao cliente" },
+];
+export const teamItems = [
+  { name: "Evgueny", job: "Programador", imgSrc: "../../public/logo.jpeg" },
+  { name: "Tatiana", job: "Administrador", imgSrc: "../../public/logo.jpeg" },
+  { name: "Alex", job: "Designer ", imgSrc: "../../public/logo.jpeg" },
+];
+export const rewiewsItems = [
+  {
+    name: "Marya",
+    text: "Com isso, o restaurante ficou mais popular!",
+    imgSrc: "../../public/logo.jpeg",
+  },
+  {
+    name: "Ana",
+    text: "O aplicativo funciona sem falhas, simplesmente super!",
+    imgSrc: "../../public/logo.jpeg",
+  },
+  {
+    name: "Michael",
+    text: "O site foi feito com rapidez e qualidade!",
+    imgSrc: "../../public/logo.jpeg",
+  },
+  {
+    name: "Pedro",
+    text: "Equipe de profissionais, recomendo a todos!",
+    imgSrc: "../../public/logo.jpeg",
+  },
+];
+
+export const questionsItems = [
+  {
+    question: "Como fazer um site rápido?",
+    answer:
+      "Se houver um layout de design, podemos lançar um site em 2 semanas!",
+  },
+  {
+    question: "E se eu não gostar?",
+    answer: "O projeto será acordado com você. Se não gostar, corrija!",
+  },
+  {
+    question: "Que tipo de negócio você atende?",
+    answer:
+      "Acima de tudo, é um pequeno negócio.  Cafés, restaurantes, oficinas, salões de beleza, etc. Todos aqueles, que precisam de resultados rápidos e um canal adicional para atrair clientes.",
+  },
+  {
+    question: "Existe suporte pós-lançamento?",
+    answer: "Certamente. Estamos sempre ligados!",
+  },
+  {
+    question: "Qual o orçamento necessário para o site?",
+    answer: "Comece a partir de 200 euros para um site estático de uma página.",
+  },
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+  rewiewsItems,
+  navItems,
+  resultsItems,
+  teamItems,
+  questionsItems,
+} from "../accets/data";
+import Drop from "../components/dropdown/drop";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,19 +17,84 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to Algarve Web
-          </h1>
-          
+    <div className="container">
+      <section className="mainSection">
+        <header className="header">
+          <div className="headerLeftBlock">
+            <img
+              src="../../public/logo-removeBG.png"
+              alt="logo"
+              className="headerImg"
+            ></img>
+            <h3>Loja de sites</h3>
+          </div>
+          <nav className="nav">
+            <ul className="navul">
+              {navItems.map((item, i) => (
+                <li className="navli" key={i}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <button className="headerBtn" type="button">
+            Encomendar site
+          </button>
         </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-         
-        </nav>
-      </div>
+        <h1 className="title">Desenvolvimento web</h1>
+        <p className="titleText">
+          Crie sua loja online, café ou escritório conosco!
+        </p>
+        <button className="titleBtn" type="button">
+          Comece agora
+        </button>
+      </section>
+      <section className="results">
+        {resultsItems.map((item, i) => (
+          <div key={i} className="resultsBlock">
+            <p className="resultsBlockTitle">{item.title}</p>
+            <h3 className="resultsBlockText">{item.text}</h3>
+          </div>
+        ))}
+      </section>
+      <section className="team">
+        <h2 className="teamTitle">Nossa equipe</h2>
+        <div className="teamContainer">
+          {teamItems.map((item, i) => (
+            <div key={i} className="teamBlock">
+              <img className="teamImg" src={item.imgSrc} alt="foto"></img>
+              <h4 className="teamName">{item.name}</h4>
+              <p className="teamJob">{item.job}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="rewiews">
+        <h2 className="rewiewsTitle">Nossa equipe</h2>
+        <div className="rewiewsContainer">
+          {rewiewsItems.map((item, i) => (
+            <div key={i} className="rewiewsBlock">
+              <img className="rewiewsImg" src={item.imgSrc} alt="foto"></img>
+              <h4 className="rewiewsName">{item.name}</h4>
+              <p className="rewiewsText">{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="transparant">
+        <span className="transparantText">
+          Crie seu site e comece a ganhar dinheiro hoje!
+        </span>
+        <button className="transparantBtn" type="button">
+          Comece agora
+        </button>
+      </section>
+      <section className="questions">
+        <h2 className="questionsTitle">Perguntas frequentes</h2>
+        {questionsItems.map((item, i) => (
+          <Drop key={i} {...item} />
+        ))}
+      </section>
     </div>
   );
 }
-
